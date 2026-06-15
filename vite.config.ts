9ee9ea57cd62bd9a77691196ac10ce9110f40058
mddunand <marianoo.14.md@gmail.com>
2026-06-14 23:33:41 -0300

    fix(config): fixing vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/fit-tracker/', // cambiá esto por el nombre exacto de tu repo
   plugins: [
     react(),
     VitePWA({
